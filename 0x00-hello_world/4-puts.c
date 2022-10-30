f73292d1e4e7f009d15de4c4cc9801e2256c029e
eilafalfatih <eilafalfatih@outlook.com>
2022-10-29 17:13:17 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main()
+{
+
+	char u[] = " Programming is like building a multinigual puzzle";
+	
+	puts(u);
+	
+	return (0);
+}
+
