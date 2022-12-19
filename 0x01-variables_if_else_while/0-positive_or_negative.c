34a309d0cff2d44ac3bd628c9dcb0447c174bb8a
eilafalfatih <eilafalfatih@outlook.com>
2022-12-19 02:31:01 -0800

    random number negative positive description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/*betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and print the 
+ * and if it is positive, negative, or zero
+ * Return: 0
+ * */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	/* your code goes there */
-	
-	if ( n>0 )
-	{
-
-		printf("%d , is positive", n);
-	}
-
-	else if ( n==0 )
-	{
-		printf("%d, is zero", n);
-	}
-	
-	else ( n<0 );
-	{
-		printf("%d, is negative", n);
-	}
-
-	return 0;
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
 }
