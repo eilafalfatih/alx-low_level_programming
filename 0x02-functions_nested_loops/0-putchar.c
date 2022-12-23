c3848f61659b8f388dc0506cedab1c752a28b2aa
eilafalfatih <eilafalfatih@outlook.com>
2022-12-24 01:08:31 +1200

    print string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - for main function
+ * descritption: the program prints a string
+ *
+ * Return :0 
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
