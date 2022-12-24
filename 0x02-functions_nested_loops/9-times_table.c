636a95d2b4644b72db0329e74ad47198376cc13b
eilafalfatih <eilafalfatih@outlook.com>
2022-12-25 03:22:04 +1200

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,13 @@ void times_table(void)
 	int a = 0;
 	int b;
 	int rep;
+
 	while (a <= 9)
 	{
 		b = 0;
 		while (b <= 9)
 		{
-			rep = a* b;
+			rep = a * b;
 			if (b == 0)
 			{
 				_putchar('0' + rep);
