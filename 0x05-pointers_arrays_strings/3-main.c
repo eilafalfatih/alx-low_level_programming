5139e07c834575150c56d12fdd3fa6bef83fbab5
eilafalfatih <eilafalfatih@outlook.com>
2022-12-30 09:36:31 +1200

    update

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -check the code
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear the computer. I fear the lack of them - Isaac asimov";
+	_puts(str);
+	return (0);
+}
