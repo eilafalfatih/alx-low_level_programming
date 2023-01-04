8fa4e9d8841a862dd5d2b67dcdd9be191f9c536f
eilafalfatih <eilafalfatih@outlook.com>
2023-01-04 13:49:29 +1200

    submit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * return: 1
+ */
+int main(void)
+{
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
+	return(1);
+}
